7f34c581bd03d12a7b3951b7b7e4608d5f4b23d1
Lucas Magno <lucasmagnorc@gmail.com>
2020-10-19 20:51:01 -0300

    Done api deliveryman validation

--- desafio02/src/app/controllers/DeliverymenController.js
+++ desafio02/src/app/controllers/DeliverymenController.js
@@ -8,17 +8,58 @@ class DeliverymenController {
     }
 
     async store(req, res) {
-        const { id, name, email } = await Deliverymen.create(req.body);
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            email: Yup.string().email().required()
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Validation fails' });
+        }
+
+        const { email, name } = req.body;
+        const deliverymanExists = await Deliverymen.findOne({ where: { email }});
+        if (deliverymanExists) {
+            return res.status(400).json({ error: 'Deliveryman already exists' });
+        }
+
+        const { id } = await Deliverymen.create(req.body);
         return res.json({ id, name, email });
     }
 
     async update(req, res) {
+        const schema = Yup.object().shape({
+            id: Yup.number().required(),
+            name: Yup.string(),
+            email: Yup.string().email()
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Validation fails' });
+        }
+
         const deliverymen = await Deliverymen.findByPk(req.body.id);
+
+        if (req.body.email !== deliverymen.email) {
+            const deliverymanExists = await Deliverymen.findOne({ where: { email: req.body.email }});
+            if (deliverymanExists) {
+                return res.status(400).json({ error: 'Deliveryman already exists' });
+            }
+        }
+
         const { id, name, avatar_id, email } = await deliverymen.update(req.body);
-        return res.json({ name, avatar_id, email });
+        return res.json({ id, name, avatar_id, email });
     }
 
     async delete(req, res) {
+        const schema = Yup.object().shape({
+            id: Yup.number().required()
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Validation fails' });
+        }
+
         const deliverymen = await Deliverymen.findByPk(req.body.id);
         await deliverymen.destroy();
         return res.json({ message: "Deliverymen was removed"});
